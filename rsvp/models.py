dcb46c30e2b65434fc6f66f43de1ae3d916c0804
ieatkittens <samuelm@gadventures.com>
2015-12-15 20:13:04 -0500

    Updated settings, added gitignore

--- rsvp/models.py
+++ rsvp/models.py
@@ -2,22 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
 class Meal(models.model):
-	name = models.CharField(max_length=50, blank=False)
-	description = models.CharField(max_length=200, blank=False)
+    name = models.CharField(max_length=50, blank=False)
+    description = models.CharField(max_length=200, blank=False)
+
 
 class Guest(models.model):
-	first_name = models.CharField(max_length=50, blank=False)
-	last_name = models.CharField(max_length=50, blank=False)
-	email = models.EmailField(blank=False)
-	phone = models.CharField(max_length=20, blank=False)
-	meal_preference = models.CharField(max_length=50, blank=False)
-	rsvp = models.BooleanField(default=False)
-	text = models.TextField(blank=True)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(blank=False)
+    phone = models.CharField(max_length=20, blank=False)
+    meal_preference = models.CharField(max_length=50, blank=False)
+    rsvp = models.BooleanField(default=False)
+    text = models.TextField(blank=True)
+
 
 class Party(models.model):
-	num_adults = models.IntegerField(default=1, null=False)
-	num_kids = models.IntegerField(default=0, null=False)
-	main_contact = models.ForeignKey(Guest)
-	secondary_contact = models.ManyToOneField(Guest)
+    num_adults = models.IntegerField(default=1, null=False)
+    num_kids = models.IntegerField(default=0, null=False)
+    primary_contact = models.ForeignKey(Guest, related_name='party')
+    secondary_contact = models.ManyToOneField(Guest, related_name='party')
--- wedding/settings.py
+++ wedding/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wedding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'weddingdb',
+        'USER': 'wedding_admin',
+        'PASSWORD': 'password1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
