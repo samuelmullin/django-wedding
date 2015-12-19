8a8f4b28527aa98deeeacbfd72929b5c7d8451da
ieatkittens <samuelm@gadventures.com>
2015-12-19 17:00:44 -0500

    Update models to include accommodation and to change auto_add_now to a timezone reference

--- rsvp/models.py
+++ rsvp/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 
 class Meal(models.Model):
@@ -8,16 +9,22 @@ class Meal(models.Model):
     description = models.CharField(max_length=200, blank=False)
 
 
+class Accommodation(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    description = models.CharField(max_length=200, blank=False)
+
+
 class Guest(models.Model):
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
-    rsvp_date = models.DateField(auto_now_add=True)
-    email = models.EmailField(blank=True)
-    phone = models.CharField(max_length=20, blank=True)
-    meal_preference = models.CharField(max_length=50, blank=False)
+    rsvp_date = models.DateField(default=timezone.now)
+    email = models.EmailField(blank=True, default='')
+    phone = models.CharField(max_length=20, blank=True, default='')
+    meal_preference = models.CharField(max_length=50, blank=True, default='')
+    accom_preference = models.CharField(max_length=50, blank=True, default='')
     primary_contact = models.BooleanField(default=True)
     rsvp = models.BooleanField(default=False)
-    text = models.TextField(blank=True)
+    text = models.TextField(blank=True, default='')
 
 
 class Party(models.Model):
