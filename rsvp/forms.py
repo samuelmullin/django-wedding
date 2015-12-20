07116a0e2789c4bab3f37e0a99c91fa17cf444d3
quaspas <daniel.quattro@gmail.com>
2015-12-20 17:16:57 -0500

    Update forms for new models

--- rsvp/forms.py
+++ rsvp/forms.py
@@ -1,82 +1,47 @@
 from django import forms
-import datetime
-from .models import Meal, Accommodation
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
-from crispy_forms.bootstrap import StrictButton
+from rsvp.models import Guest
 
 
-class PartyForm(forms.Form):
+class PartyForm(forms.ModelForm):
+    name = forms.CharField(label='Name')
+    email = forms.EmailField(label='Email')
+
+    rsvp = forms.ChoiceField(
+        label='Can you make it?',
+        widget=forms.RadioSelect(attrs={}),
+        choices=[
+            (True, "We can make it"),
+            (False, "We can't make it")
+        ],
+    )
+
     num_adults = forms.IntegerField(
-        label="Number of Adults",
+        label="How many adults?",
         widget=forms.NumberInput(
             attrs={
                 "class": 'form-control',
                 "placeholder": "12 and older",
             }))
+
     num_kids = forms.IntegerField(
-        label="Number of Kids (under 12)",
+        label="How many Kids? (under 12)",
         widget=forms.NumberInput(
             attrs={
                 "class": 'form-control',
                 "placeholder": "11 and younger",
             }))
-    rsvp = forms.ChoiceField(
-        label='Can you make it?',
-        widget=forms.RadioSelect(
-        attrs={
-            "class": 'form-control radio-inline'
-        }),
-        choices=[
-            (True, "We can make it"),
-            (False, "We can't make it")
-        ],
-        )
 
-    def __init__(self, *args, **kwargs):
-        super(PartyForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'form-control col-sm-2 col-md-2 col-lg-2'
-        self.helper.field_class = 'form-control col-sm-8 col-md-8 col-lg-8'
-        self.helper.layout = Layout(
-            'num_adults',
-            'num_kids',
-            'rsvp',
-            Submit('submit', 'Next', css_class='btn-default'),
-        )
+    class Meta:
+        model = Guest
+        fields = ['rsvp', 'name', 'email', 'meal_preference', 'accom_preference']
 
 
-class GuestForm(forms.Form):
-    first_name = forms.CharField(
-        label='First Name'
-    )
-    last_name = forms.CharField(
-        label='Last Name'
-    )
-    email = forms.EmailField(
-        label='Email'
-    )
-    phone = forms.CharField(
-        label='Phone Number'
-    )
-    rsvp = forms.BooleanField(
-        label='Can you Make it?',
-        widget=forms.Select(
-            choices=[
-                (True, 'Yes'),
-                (False, 'No')
-            ],
-            attrs={
-                "class": 'form-control radio-inline'
-            }))
-    text = forms.CharField(
-        label='Comments',
-        widget=forms.TextInput(
-            attrs={
-                "class": 'form-control',
-                "placeholder": "Extra Details - Allergies, etc!"
-            }))
+class AdultForm(forms.ModelForm):
+
+    name = forms.CharField(label='Name')
+    email = forms.EmailField(label='Email')
+    phone = forms.CharField(label='Phone Number')
+
     meal_choice = forms.ChoiceField(
         label='Meal Choice',
         choices=[],
@@ -93,21 +58,46 @@ class GuestForm(forms.Form):
                 "class": 'form-control',
             }))
 
-    def __init__(self, *args, **kwargs):
-        super(GuestForm, self).__init__(*args, **kwargs)
-        self.fields['meal_choice'].choices = [(value, value) for value in Meal.objects.all().order_by('name').values_list('name', flat=True)]
-        self.fields['accom_choice'].choices = [(value, value) for value in Accommodation.objects.all().order_by('name').values_list('name', flat=True)]
-        self.helper = FormHelper(self)
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'form-control col-sm-2'
-        self.helper.field_class = 'form-control col-sm-8'
-        self.helper.layout = Layout(
-            'first_name',
-            'last_name',
-            'email',
-            'phone',
-            'rsvp',
-            'meal_choice',
-            'accom_choice',
-            Submit('submit', 'Next', css_class='btn-default'),
-        )
+    text = forms.CharField(
+        label='Comments',
+        widget=forms.TextInput(
+            attrs={
+                "class": 'form-control',
+                "placeholder": "Extra Details - Allergies, etc!"
+            }))
+
+    class Meta:
+        model = Guest
+        fields = ['name', 'meal_choice',]
+
+
+class KidForm(forms.Form):
+    name = forms.CharField(label='Name')
+
+    meal_choice = forms.ChoiceField(
+        label='Meal Choice',
+        choices=[],
+        widget=forms.Select(
+            attrs={
+                "class": 'form-control',
+            }))
+
+    accom_choice = forms.ChoiceField(
+        label='Accommodation Choice',
+        choices=[],
+        widget=forms.Select(
+            attrs={
+                "class": 'form-control',
+            }))
+
+    text = forms.CharField(
+        label='Comments',
+        widget=forms.TextInput(
+            attrs={
+                "class": 'form-control',
+                "placeholder": "Extra Details - Allergies, etc!"
+            }))
+
+    class Meta:
+        model = Guest
+        fields = ['name', 'meal_choice', 'accom_choice', 'text']
